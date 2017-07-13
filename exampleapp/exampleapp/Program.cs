da8dd5cdf45c9f099e823910a84fc64803f3bd26
Richard Horne <Richard.Horne@dcc.govt.nz>
2017-07-13 15:34:39 +1200

    Initial commit

---
+++ exampleapp/exampleapp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace exampleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
